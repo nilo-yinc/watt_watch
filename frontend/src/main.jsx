ddd4780d67087fb3f8e755b22b790acb316ec978
Niloy Mallik <niloymallik0001@gmail.com>
2026-03-05 13:31:45 +0530

    use HashRouter to prevent refresh 404 on Vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 import { WebSocketProvider } from './context/WebSocketContext';
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <ThemeProvider>
                 <AppProvider>
                     <WebSocketProvider>
@@ -17,6 +17,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                     </WebSocketProvider>
                 </AppProvider>
             </ThemeProvider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
